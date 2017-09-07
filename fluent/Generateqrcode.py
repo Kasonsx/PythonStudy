c163b48544d178a7bbcb51dfe261724c0b24031f
Kasonsx <sxk644653293@163.com>
2017-09-07 16:23:10 +0800

    qrcode

---
+++ fluent/Generateqrcode.py
@@ -0,0 +1,21 @@
+#生成二维码
+import qrcode
+#img = qrcode.make('This is a quick response code!')
+
+def generate(data):#貌似只能接受字符串，网址链接需要用双引号
+	qr = qrcode.QRCode(
+		version=1,
+		error_correction=qrcode.constants.ERROR_CORRECT_L,#容错率
+		box_size=10,
+		border=4,
+	)
+	print(type(data))
+	qr.add_data(data)
+	qr.make(fit=True)
+	img = qr.make_image(fill_color="white",back_color="black")
+	img.save('qrcode.jpg','jpeg')
+	img.show()
+
+if __name__ == '__main__':
+	import sys
+	generate(*sys.argv[1:])
